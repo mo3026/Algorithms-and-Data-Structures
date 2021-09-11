31524b531f5755f65b924692ae15e462983703a0
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-11 11:14:17 -0500

    Update House Robber (Leetcode 198).cs

--- Dynamic Programming/House Robber (Leetcode 198).cs
+++ Dynamic Programming/House Robber (Leetcode 198).cs
@@ -1,1 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int Rob(int[] nums)
+        {
+            int rob1=0, rob2=0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int newRob = Math.Max(rob1 + nums[i], rob2);
+                rob1 = rob2;
+                rob2 = newRob;
+            }
+            return rob2;
+        }
+    }
+}
