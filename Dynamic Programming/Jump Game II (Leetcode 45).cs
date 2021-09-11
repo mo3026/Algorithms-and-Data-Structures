338e220e73e575020ef3c8afc47950db75205fdd
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-11 11:15:36 -0500

    Update Jump Game II (Leetcode 45).cs

--- Dynamic Programming/Jump Game II (Leetcode 45).cs
+++ Dynamic Programming/Jump Game II (Leetcode 45).cs
@@ -1,1 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int Jump(int[] nums)
+        {
+            int[] jump = new int[nums.Length];
+            jump[nums.Length - 1] = 1;
+            for (int i = jump.Length - 2; i >= 0; i--)
+            {
+                int min = nums.Length;
+                for (int j = nums[i]; j >0; j--)
+                {
+                    if (i + j< nums.Length && jump[i + j] != 0)
+                    {
+                        if (min > jump[i + j]) min = jump[i + j];
+                    }
+                }
+                jump[i] = min + 1;
+            }
+            return jump[0]-1;
+        }
+    }
+}
