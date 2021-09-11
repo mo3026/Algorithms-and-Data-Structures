bb053367ab40ac7d3477a18f64461cae42947724
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-11 11:55:39 -0500

    Create Two Sum (Leetcode 1).cs

---
+++ Arrays/Two Sum (Leetcode 1).cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = i + 1; j < nums.Length; j++)
+                    if (nums[i] + nums[j] == target)
+                        return new int[2] { i, j };
+            return new int[0] { };
+        }
+    }
+}
