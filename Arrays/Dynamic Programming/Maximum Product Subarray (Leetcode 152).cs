7fa6cb1f8ae7e29032fb40d3cf42d39c26da464b
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-12 11:08:45 -0500

    Create Maximum Product Subarray (Leetcode 152).cs

---
+++ Arrays/Dynamic Programming/Maximum Product Subarray (Leetcode 152).cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //https://www.youtube.com/watch?v=lXVy6YWFcRM
+        public static int MaxProduct(int[] nums)
+        {
+            int result = nums.Max();
+            int max = 1;
+            int min = 1;
+            foreach (int num in nums)
+            {
+                if (num == 0)
+                {
+                    max = 1;
+                    min = 1;
+                    continue;
+                }
+                int temp = max * num;
+                max = new List<int> { num * max, num * min, num }.Max();
+                min = new List<int> { temp, num * min, num }.Min();
+                result = Math.Max(result, max);
+            }
+            return result;
+        }
+    }
+}
