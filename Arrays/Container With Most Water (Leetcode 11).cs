56da1d59e36386de44566e3ba58e70267738dba0
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-15 14:27:14 -0500

    Update Container With Most Water (Leetcode 11).cs

--- Arrays/Container With Most Water (Leetcode 11).cs
+++ Arrays/Container With Most Water (Leetcode 11).cs
@@ -1,1 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int MaxArea(int[] height)
+        {
+            int maxLeft = 0;
+            int maxRight = height.Length - 1;
+            int MaxArea = (maxRight - maxLeft) * (Math.Min(height[maxRight], height[maxLeft]));
+            while (maxRight != maxLeft)
+            {
+                if (maxLeft < maxRight)
+                {
+                    maxLeft = maxLeft + 1;
+                    int temp = (maxRight - maxLeft) * (Math.Min(height[maxRight], height[maxLeft]));
+                    if (temp > MaxArea) MaxArea = temp;
+                }
+                else
+                {
+                    maxRight = maxRight - 1;
+                    int temp = (maxLeft - maxRight) * (Math.Min(height[maxRight], height[maxRight]));
+                    if (temp > MaxArea) MaxArea = temp;
+                }
+            }
+            return MaxArea;
+        }
+    }
+}
