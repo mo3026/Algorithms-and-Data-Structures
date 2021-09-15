28cf800af24e782d7e77835ba79435935d14f359
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-15 14:12:42 -0500

    Update Longest Palindromic Substringcs (Leetcode 5).cs

--- Strings/Longest Palindromic Substringcs (Leetcode 5).cs
+++ Strings/Longest Palindromic Substringcs (Leetcode 5).cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static string LongestPalindrome(string s)
+        {
+            string LongestPalindrome = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    string temp = s.Substring(i, j - i + 1);
+                    char[] chars = temp.ToCharArray();
+                    Array.Reverse(chars);
+                    string reverse = new string(chars);
+                    if (temp == reverse)
+                    {
+                        if (temp.Length > LongestPalindrome.Length) LongestPalindrome = temp;
+                    }
+                }
+            }
+            return LongestPalindrome;
+        }
+    }
+}
