2313d015faf4a86aa830fd9e94184a966f97bca7
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-14 20:49:23 -0500

    Create Implement Trie (Prefix Tree) (Leetcode 208).cs

---
+++ Trees/Trie (Prefix Tree)/Implement Trie (Prefix Tree) (Leetcode 208).cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        //https://www.youtube.com/watch?v=oobqoCJlHA0
+        public class Trie
+        {
+            private Trie[] Children = new Trie[26];
+            private string word;
+
+            /** Initialize your data structure here. */
+            public Trie()
+            {
+            }
+
+            /** Inserts a word into the trie. */
+            public void Insert(string word)
+            {
+                if (word == null || word.Length == 0)
+                    return;
+
+                var root = this;
+
+                foreach (var item in word)
+                {
+                    if (root.Children[item - 'a'] == null)
+                    {
+                        root.Children[item - 'a'] = new Trie();
+                    }
+
+                    root = root.Children[item - 'a'];
+                }
+
+                root.word = word;
+            }
+
+            /** Returns if the word is in the trie. */
+            public bool Search(string word)
+            {
+                if (word == null || word.Length == 0)
+                    return false;
+
+                var root = this;
+
+                foreach (var item in word)
+                {
+                    if (root.Children[item - 'a'] == null)
+                    {
+                        return false;
+                    }
+
+                    root = root.Children[item - 'a'];
+                }
+
+                return root.word != null;
+            }
+
+            /** Returns if there is any word in the trie that starts with the given prefix. */
+            public bool StartsWith(string prefix)
+            {
+                if (prefix == null || prefix.Length == 0)
+                    return false;
+
+                var root = this;
+
+                foreach (var item in prefix)
+                {
+                    if (root.Children[item - 'a'] == null)
+                    {
+                        return false;
+                    }
+
+                    root = root.Children[item - 'a'];
+                }
+
+                return true;
+            }
+        }
+    }
+}
