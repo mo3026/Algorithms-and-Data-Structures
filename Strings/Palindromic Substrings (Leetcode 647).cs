3787e0c2397024f404afc8f767a5f961a01f8a74
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-15 14:14:03 -0500

    Update Palindromic Substrings (Leetcode 647).cs

--- Strings/Palindromic Substrings (Leetcode 647).cs
+++ Strings/Palindromic Substrings (Leetcode 647).cs
@@ -1,1 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        int countSubstrings(string s)
+        {
+            int ans = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                ans += count(s, i, i); // odd length
+                ans += count(s, i, i + 1); // even length
+            }
+            return ans;
+        }
+
+        static int count(string s, int l, int r)
+        {
+            int ans = 0;
+            while (l >= 0 & r < s.Length && s[l--] == s[r++])
+                ++ans;
+            return ans;
+        }
+    }
+}
