3d51e9c7e83c378aad8346c67f6c0f46b378a216
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-14 20:59:50 -0500

    Create Same Tree (Leetcode 100).cs

---
+++ Trees/Same Tree (Leetcode 100).cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if (p == null) return false;
+            if (q == null) return false;
+            if (p.val != q.val) return false;
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
