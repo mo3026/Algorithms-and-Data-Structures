a0c3cf7dfd4e4569b41aeb615b48c251a4577dd4
Mohammadsaleh Gharehdaghi <47196591+mo3026@users.noreply.github.com>
2021-09-15 14:17:17 -0500

    Update Valid Parentheses (Leetcode 20).cs

--- Strings/Valid Parentheses (Leetcode 20).cs
+++ Strings/Valid Parentheses (Leetcode 20).cs
@@ -1,1 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<Char> stack = new Stack<Char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+                if (s.Length == 0) return false;
+                if (s[i] == '(')
+                {
+                    stack.Push(')');
+                }
+                else if (s[i] == '{')
+                {
+                    stack.Push('}');
+                }
+                else if (s[i] == '[')
+                {
+                    stack.Push(']');
+                }
+                else if (stack.Count == 0)
+                    return false;
+                else if (s[i] == stack.Peek())
+                {
+                    stack.Pop(); Console.WriteLine("pop");
+                }
+                else
+                    return false;
+            }
+            if (stack.Count == 0)
+                return true;
+            return false;
+        }
+    }
+}
